747287d4b9aa1bdafee9a5fa8caf2e0675b19441
evanmalmud <evanmalmud@gmail.com>
2021-04-25 02:00:57 -0400

    Adding Time UI and Pixel Art Font to all Menus

--- Assets/Archived Project/Scripts/GameManager/LevelTimer.cs
+++ Assets/Archived Project/Scripts/GameManager/LevelTimer.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class LevelTimer : MonoBehaviour
@@ -12,6 +13,8 @@ public class LevelTimer : MonoBehaviour
     public bool counting = false;
 
     public GameState gameState;
+
+    public TextMeshProUGUI text;
     
     // Start is called before the first frame update
     void Start()
@@ -30,6 +33,7 @@ public class LevelTimer : MonoBehaviour
             counting = false;
             gameState.timesUp();
         }
+        text.text = "T-" + (int)levelTimeLeft;
     }
 
     public float getLevelTimeLeft() {
