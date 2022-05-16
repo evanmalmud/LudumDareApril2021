b87dc12653e78e1ffdbc99e0dd9410eea2a0d047
evanmalmud <evanmalmud@gmail.com>
2022-05-16 02:49:56 -0400

    Added Script to making upgrade shop tiles

--- Assets/Art-Jin/UpgradesScript.cs
+++ Assets/Art-Jin/UpgradesScript.cs
@@ -1,18 +1,75 @@
+using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class UpgradesScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public List<GameObject> backgrounds;
+
+    [ValueDropdown("backgrounds")]
+    [OnValueChanged("updateBackground")]
+    public GameObject background;
+
+
+    public List<GameObject> symbols;
+
+    [ValueDropdown("symbols")]
+    [OnValueChanged("updateSymbols")]
+    public GameObject symbol;
+
+
+    public List<GameObject> arrows;
+
+    [ValueDropdown("arrows")]
+    [OnValueChanged("updateArrows")]
+    public GameObject arrow;
+
+    public void updateGameObjects()
     {
-        
+        updateBackground();
+        updateSymbols();
+        updateArrows();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void updateBackground()
     {
-        
+        foreach(GameObject go in backgrounds)
+        {
+            go.SetActive(false);
+        }
+        if(background != null)
+        {
+            background.SetActive(true);
+        }
     }
+
+    private void updateSymbols()
+    {
+        foreach (GameObject go in symbols)
+        {
+            go.SetActive(false);
+        }
+        if (symbol != null)
+        {
+            symbol.SetActive(true);
+        }
+    }
+
+    private void updateArrows()
+    {
+        foreach (GameObject go in arrows)
+        {
+            go.SetActive(false);
+        }
+        if (arrow != null)
+        {
+            arrow.SetActive(true);
+        }
+    }
+
+
+
+
 }
